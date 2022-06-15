8f470c012611f86b18f5fb5c7236ab23c096787f
Minh Duc <minhduc@Minhs-MacBook-Pro.local>
2022-06-15 11:01:39 +0700

    fix bug

--- src/views/user/Login.vue
+++ src/views/user/Login.vue
@@ -1,89 +1,100 @@
 <template>
-  <div class="main">
-    <a-form-model
-      class="user-layout-login"
-      ref="formLogin"
-      name="formLogin"
-      :model="form"
-    >
-      <a-alert v-if="isLoginError" type="error" showIcon style="margin-bottom: 24px;" :message="$t('user.login.message-invalid-credentials')" />
-      <a-form-model-item
-        prop="userName">
-        <a-input
-          size="large"
-          type="text"
-          :placeholder="'Tài khoản'"
-          v-model="form.userName"
-          style="margin-top: 15px"
-        >
-          <a-icon slot="prefix" type="user" :style="{ color: 'rgba(0,0,0,.25)' }"/>
-        </a-input>
-      </a-form-model-item>
-
-      <a-form-model-item
-        prop="password">
-        <a-input-password
-          size="large"
-          :placeholder="'Mật khẩu'"
-          v-model="form.password"
-          style="margin-top: 15px"
-        >
-          <a-icon slot="prefix" type="lock" :style="{ color: 'rgba(0,0,0,.25)' }"/>
-        </a-input-password>
-      </a-form-model-item>
-      <a-form-model-item
-        label="Mã xác nhận"
-        prop="captcha"
-      >
-        <div style="margin-top: 15px">
-          <a-row :gutter="16">
-            <a-col :xs="12" :md="12" :lg="12">
-              <a-input
-                size="large"
-                :placeholder="'Mã xác nhận'"
-                v-model="form.captcha"
+  <blank-layout>
+    <div class="container">
+      <div class="limiter">
+        <div class="container-login">
+          <div class="wrap-login">
+            <a-row :gutter="16" style="width: 100%;">
+              <img src="~@/assets/Logo_VNA.svg" class="logo" alt="logo">
+              <a-form-modelx
+                ref="ruleFormLogin"
+                :model="form"
+                class="user-layout-login"
               >
-              </a-input>
-            </a-col>
-            <a-col :xs="12" :md="12" :lg="12">
-              <div style="display: flex; justify-content: flex-start">
-                <div>
-                  <img :src="captcha.captcha" style="width: 100px; height: auto" alt="">
-                </div>
-                <div>
-                  <a-button type="default" style="margin-left: 8px" @click="getCaptcha"><a-icon type="sync"></a-icon></a-button>
-                </div>
-              </div>
-            </a-col>
-          </a-row>
-        </div>
-      </a-form-model-item>
-      <a-form-model-item>
-        <a-button
-          size="large"
-          type="primary"
-          htmlType="submit"
-          class="login-button"
-          :loading="loginBtn"
-          :disabled="loginBtn"
-          style="margin-top:24px"
-          @click="handleSubmit"
-        >{{ $t('user.login.login') }}</a-button>
-      </a-form-model-item>
+                <a-form-model-item
+                  label="Tài khoản"
+                  prop="userName">
+                  <a-input
+                    size="large"
+                    type="text"
+                    :placeholder="'Tài khoản'"
+                    v-model="form.userName"
+                    style="margin-top: 15px"
+                  >
+                    <a-icon slot="prefix" type="user" :style="{ color: 'rgba(0,0,0,.25)' }"/>
+                  </a-input>
+                </a-form-model-item>
 
-    </a-form-model>
-  </div>
+                <a-form-model-item
+                  label="Mật khẩu"
+                  prop="password">
+                  <a-input-password
+                    size="large"
+                    :placeholder="'Mật khẩu'"
+                    v-model="form.password"
+                    style="margin-top: 15px"
+                  >
+                    <a-icon slot="prefix" type="lock" :style="{ color: 'rgba(0,0,0,.25)' }"/>
+                  </a-input-password>
+                </a-form-model-item>
+                <a-form-model-item
+                  label="Mã xác nhận"
+                  prop="captcha"
+                >
+                  <div style="margin-top: 15px">
+                    <a-row :gutter="16">
+                      <a-col :xs="12" :md="12" :lg="12">
+                        <a-input
+                          size="large"
+                          :placeholder="'Mã xác nhận'"
+                          v-model="form.captcha"
+                        >
+                        </a-input>
+                      </a-col>
+                      <a-col :xs="12" :md="12" :lg="12">
+                        <div style="display: flex; justify-content: flex-start">
+                          <div>
+                            <img :src="captcha.captcha" style="width: 100px; height: auto" alt="">
+                          </div>
+                          <div>
+                            <a-button type="default" style="margin-left: 8px" @click="getCaptcha"><a-icon type="sync"></a-icon></a-button>
+                          </div>
+                        </div>
+                      </a-col>
+                    </a-row>
+                  </div>
+                </a-form-model-item>
+                <a-form-model-item>
+                  <a-button
+                    size="large"
+                    type="primary"
+                    htmlType="submit"
+                    class="login-button"
+                    :loading="loginBtn"
+                    :disabled="loginBtn"
+                    style="margin-top:24px"
+                    @click="handleSubmit"
+                  >Đăng nhập</a-button>
+                </a-form-model-item>
+
+              </a-form-modelx>
+            </a-row>
+          </div>
+        </div>
+      </div>
+    </div>
+  </blank-layout>
 </template>
 
 <script>
-import TwoStepCaptcha from '@/components/tools/TwoStepCaptcha'
 import { mapActions } from 'vuex'
 import { timeFix } from '@/utils/util'
 import { getCaptcha, login } from '@/api/login'
+import BlankLayout from '@/layouts/BlankLayout'
 
 export default {
   components: {
-    TwoStepCaptcha
+    BlankLayout
   },
   data () {
     return {
@@ -94,52 +105,49 @@ export default {
         captcha: '',
         transactionId: ''
       },
-      isLoginError: false,
       captcha: null
+
     }
   },
-  created () {
-    this.getCaptcha()
+  async created () {
+    await this.getCaptcha()
   },
   methods: {
     ...mapActions(['Login', 'Logout']),
     handleSubmit () {
-      this.loginBtn = true
-      const params = {
-        userName: this.form.userName,
-        password: this.form.password,
-        captcha: this.form.captcha,
-        transactionId: this.form.transactionId
-      }
-      console.log(params)
-      this.$refs.formLogin.validate(valid => {
+      console.log(1)
+      this.$refs.ruleFormLogin.validate(valid => {
         if (valid) {
-          console.log(valid)
+          console.log(2)
+          this.loginBtn = true
+          const params = {
+            userName: this.form.userName,
+            password: this.form.password,
+            captcha: this.form.captcha,
+            transactionId: this.form.transactionId
+          }
           login(params).then((res) => this.loginSuccess(res))
             .catch(err => this.requestFailed(err))
             .finally(() => {
               this.loginBtn = false
-          })
+            })
         }
       })
     },
     loginSuccess (res) {
       console.log(res)
       this.$router.push({ path: '/' })
-      // 延迟 1 秒显示欢迎信息
       setTimeout(() => {
         this.$notification.success({
-          message: '欢迎',
-          description: `${timeFix()}，欢迎回来`
+          message: 'Lỗi',
+          description: `${timeFix()}，Chào mừng trở lại`
         })
       }, 1000)
-      this.isLoginError = false
     },
     requestFailed (err) {
-      this.isLoginError = true
       this.$notification['error']({
-        message: '错误',
-        description: ((err.response || {}).data || {}).message || '请求出现错误，请稍后再试',
+        message: 'Lỗi',
+        description: ((err.response || {}).data || {}).message,
         duration: 4
       })
     },
@@ -159,12 +167,6 @@ export default {
 .user-layout-login {
   label {
     font-size: 14px;
-  }
-
-  .getCaptcha {
-    display: block;
-    width: 100%;
-    height: 40px;
   }
 
   .forge-password {
